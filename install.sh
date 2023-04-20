6f06eda4ab99cc4399b9d81eaff1f2cc97254819
Omar <31930599+omariscoming@users.noreply.github.com>
2023-04-20 18:14:17 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ read -p "Record Domain: " domain
 ssh_command="
 apt install curl
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) -n
+scp root@$OLDONE:/etc/x-ui/x-ui.db /etc/x-ui/
 sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
     cp /etc/nginx/sites-available/default /etc/nginx/sites-available/${domain}
     ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
@@ -15,10 +16,9 @@ sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
     sed -i \"21 r /root/ReverseProxy_v2ray/reverse.txt\" \"/etc/nginx/sites-available/${domain}\"
     certbot --nginx --agree-tos --no-eff-email --redirect --expand --force-renewal -d ${domain} --register-unsafely-without-email
     systemctl restart nginx
+    x-ui restart
 "
 ssh -o StrictHostKeyChecking=no "root@$NEWONE" "$ssh_command"
-scp root@$OLDONE:/etc/x-ui/x-ui.db root@$NEWONE:/etc/x-ui/
-x-ui restart
-}
 
+}
 changeserver
