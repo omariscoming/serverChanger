24d068d28508ca3fe6cf145fc4d0d17bd5972900
Omar <31930599+omariscoming@users.noreply.github.com>
2023-04-20 17:55:25 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 function changeserver(){
 sudo apt-get install sshpass jq -y
 read -p "OldServer: " OLDONE
-read -p "old Server password: " OLDPASS
 read -p "NewServer: " NEWONE
-read -p "new Server password: " NEWPASS
 read -p "Record Domain: " domain
 
 ssh_command="
 apt install curl
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) -n
 sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
     cp /etc/nginx/sites-available/default /etc/nginx/sites-available/${domain}
     ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
@@ -17,10 +15,10 @@ sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
     sed -i \"21 r /root/ReverseProxy_v2ray/reverse.txt\" \"/etc/nginx/sites-available/${domain}\"
     certbot --nginx --agree-tos --no-eff-email --redirect --expand --force-renewal -d ${domain} --register-unsafely-without-email
     systemctl restart nginx
-    	sshpass -p "$OLDPASS" scp -r root@$OLDONE:/etc/x-ui/x-ui.db /etc/x-ui/
-	x-ui restart
 "
-sshpass -p "$NEWPASS" ssh -o StrictHostKeyChecking=no "root@$NEWONE" "$ssh_command"
+ssh -o StrictHostKeyChecking=no "root@$NEWONE" "$ssh_command"
+scp root@$OLDONE:/etc/x-ui/x-ui.db root@$NEWONE:/etc/x-ui/
+x-ui restart
 }
 
 changeserver
