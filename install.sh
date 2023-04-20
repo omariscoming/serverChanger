c250329867ea844a75218d10f158dc4712d2c4a1
Omar <31930599+omariscoming@users.noreply.github.com>
2023-04-20 14:09:23 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+function changeserver(){
+read -p "OldServer: " OLDONE
+read -p "NewServer: " NEWONE
+read -p "Record Domain: " domain
+
+ssh_command="
+apt install curl
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+scp root@$OLDONE:/etc/x-ui/x-ui.db /etc/x-ui/
+sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
+    cp /etc/nginx/sites-available/default /etc/nginx/sites-available/${domain}
+    ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
+    sed -i \"s/_;/${domain};/\" \"/etc/nginx/sites-available/${domain}\"
+    sed -i \"s/ default_server//\" \"/etc/nginx/sites-available/${domain}\"
+    sed -i \"21 r /root/ReverseProxy_v2ray/reverse.txt\" \"/etc/nginx/sites-available/${domain}\"
+    certbot --nginx --agree-tos --no-eff-email --redirect --expand --force-renewal -d ${domain} --register-unsafely-without-email
+    systemctl restart nginx
+	x-ui restart
+"
+sshpass -p "" ssh -o StrictHostKeyChecking=no "root@$NEWONE" "$ssh_command"
+}
+
+changeserver
